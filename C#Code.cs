b303742f1d6ceb1203b04208ea82e0611bde1d4c
florian555666 <florian.partsch@web.de>
2025-02-25 12:54:06 +0000

    h

---
+++ C#Code.cs
@@ -0,0 +1,18 @@
+using System;
+
+class C#Code{
+    public static void Main(string[] args){
+        Start();
+
+        
+    }
+    public void Start(){
+        Console.Writeline("Programm wurde erfolgreich ausgeführt")
+
+
+        
+    }
+
+
+    
+}
